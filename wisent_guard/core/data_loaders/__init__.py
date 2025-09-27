4fafe16823482714da20c2bff54596d841984143
gracjangoral <gracjan@wisent.ai>
2025-09-27 18:59:38 +0200

    - removing unused or unnecessary files

--- wisent_guard/core/data_loaders/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Data loaders for various benchmarks and datasets.
-"""
-
-from .livecodebench_loader import LiveCodeBenchLoader
-from .steering_data_extractor import SteeringDataExtractor
-
-__all__ = ["LiveCodeBenchLoader", "SteeringDataExtractor"]
